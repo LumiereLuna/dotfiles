dff594315acab2c8a454e96aa82000ccde59e92e
Selene Tamez Galván <tg.sln255@gmail.com>
2026-05-10 16:39:08 -0600

    neovim: switch colourscheme

--- neovim/.config/nvim/lua/custom/plugins/theme.lua
+++ neovim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,21 +1,7 @@
 return {
-    'kungfusheep/mfd.nvim',
-    lazy = false,
+    'olimorris/onedarkpro.nvim',
     priority = 1000,
     config = function()
-        require('mfd').setup({
-            accessibility_contrast = 7,
-        })
-        vim.cmd.colorscheme('mfd-lumon')
-
-        vim.opt.guicursor = {
-            'n:block-CursorNormal',
-            'v:block-CursorVisual',
-            'i:block-CursorInsert',
-            'r-cr:block-CursorReplace',
-            'c:block-CursorCommand',
-        }
-
-        require('mfd').enable_cursor_sync()
+        vim.cmd.colorscheme('onedark_dark')
     end,
 }
